e0ddd1bf09631c5ed437d0d3cdb91f3b0a456bf6
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-14 15:09:55 -0500

    /getKoala sends back db results

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ var path = require( 'path' );
 var bodyParser= require( 'body-parser' );
 var urlencodedParser = bodyParser.urlencoded( {extended: false } );
 var port = process.env.PORT || 8080;
+var pg = require('pg');
+var connectionString = 'postgress://localhost:5432/koalaHollaDB';
 // static folder
 app.use( express.static( 'public' ) );
 
@@ -22,12 +24,23 @@ app.get( '/', function( req, res ){
 app.get( '/getKoalas', function( req, res ){
   console.log( 'getKoalas route hit' );
   //assemble object to send
-  var objectToSend={
-    response: 'from getKoalas route'
-  }; //end objectToSend
-  //send info back to client
-  res.send( objectToSend );
-});
+  pg.connect(connectionString, function(err, client, done){
+    if (err){
+      console.log(err);
+    }
+    else{
+      var resultsArray = [];
+      var queryResults = client.query('SELECT * FROM koalas');
+      queryResults.on('row', function(row){
+        resultsArray.push(row);
+      });
+      queryResults.on('end', function(){
+        done();
+        return res.json(resultsArray);
+      });
+    }//end else
+  });//end connect
+});//end /getKoalas
 
 // add koala
 app.post( '/addKoala', urlencodedParser, function( req, res ){
